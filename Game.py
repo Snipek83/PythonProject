5603f80ffa6ca76c46ce1a3c53776afee02edb66
Snipek83 <Snipe83@mail.ru>
2025-02-27 20:46:32 +0300

    sq

---
+++ Game.py
@@ -0,0 +1,65 @@
+import pygame
+
+WIDTH = 600
+HEIGHT = 800
+
+FPS = 60
+
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 40))
+        self.image.fill(GREEN)
+        self.image = pygame.Surface((50, 40))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+
+    def update(self):
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -5
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 5
+        self.rect.x += self.speedx
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+                self.rect.left = 0
+
+
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+#screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My Snipe Game")
+clock = pygame.time.Clock()
+
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+
+running = True
+while running:
+    clock.tick(FPS)
+    for event in pygame.event.get():  #кнопка крестик
+        if event.type == pygame.QUIT: #кнопка крестик
+            running = False           #кнопка крестик
+    screen.fill(BLACK)
+
+    all_sprites.update()
+    all_sprites.draw(screen)
+    pygame.display.flip()  #заливка черным цветом
+pygame.quit()
+
