12edef4eb9454728bc39a89d6f8b1ad222bae62a
Snipek83 <Snipe83@mail.ru>
2025-03-03 19:44:29 +0300

    Создание обьекта "Пуля"

--- Game.py
+++ Game.py
@@ -23,8 +23,6 @@ class Player(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((50, 40))
         self.image.fill(GREEN)
-        self.image = pygame.Surface((50, 40))
-        self.image.fill(GREEN)
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - INDENT
@@ -43,6 +41,12 @@ class Player(pygame.sprite.Sprite):
         if self.rect.left < INDENT:
             self.rect.left = INDENT
 
+    def shoot(self):
+        bullet = Bullet()
+        bullets.add(bullet)
+        all_sprites.add (bullet)
+
+
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -58,12 +62,26 @@ class Enemy(pygame.sprite.Sprite):
     def update(self):
         self.rect.x += self.speedx
         self.rect.y += self.speedy
-        if self.rect.top > HEIGHT:
+        if self.rect.top > HEIGHT or self.rect.right < 0 or self.rect.left > WIDTH:
             self.rect.x = random.randrange(WIDTH - self.rect.width)
             self.rect.y = random.randrange(-100, -30)
             self.speedx = random.randrange(-3, 4)
             self.speedy = random.randrange(1, 8)
 
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((10, 20))
+        self.image.fill(BLUE)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = player.rect.centerx
+        self.rect.bottom = player.rect.y
+        self.speedy = - 10
+
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.bottom < 0:
+            self.kill()
 
 
 pygame.init()
@@ -74,6 +92,8 @@ clock = pygame.time.Clock()
 
 all_sprites = pygame.sprite.Group()
 enemies = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
+
 player = Player()
 all_sprites.add(player)
 
@@ -90,12 +110,16 @@ while running:
     clock.tick(FPS)
     for event in pygame.event.get():  # кнопка крестик
         if event.type == pygame.QUIT:  # кнопка крестик
-            running = False  # кнопка крестик
+            running = False
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE :
+            player.shoot()
     all_sprites.update()
 
     hits = pygame.sprite.spritecollide(player, enemies, False)
     if hits:
         running = False
+
+    hits = pygame.sprite.groupcollide(enemies, bullets, True, True)
     screen.fill(BLACK)
     all_sprites.draw(screen)
     pygame.display.flip()  # заливка черным цветом
